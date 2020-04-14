e63e83ff95608ed00942196f648fe858bc17af98
Simar Singh <simar@codejudge.io>
2020-04-14 16:18:43 +0530

    remove add 2 nums

--- src/main/java/org/codejudge/sb/controller/AppController.java
+++ src/main/java/org/codejudge/sb/controller/AppController.java
@@ -1,7 +1,5 @@
 package org.codejudge.sb.controller;
 
-import org.codejudge.sb.model.SumRequest;
-import org.codejudge.sb.model.SumResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +11,6 @@ public class AppController {
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity helloWorld() {
-        return new ResponseEntity<>("Hello World!!", HttpStatus.OK);
-    }
-
-    @PostMapping(value = "/api/add", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity add2Nums(@RequestBody SumRequest request) {
-        SumResponse sumResponse = new SumResponse();
-        sumResponse.setSum(request.getNumber1() + request.getNumber2());
-        return new ResponseEntity<>(sumResponse, HttpStatus.OK);
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
     }
 }
